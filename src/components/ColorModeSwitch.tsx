5883e5ad8e78916c7b8555d6a29b515d4fd00d94
Tanapon Piasuwan <paisuwan16@gmail.com>
2023-12-27 15:33:52 +0700

    build the color mode button

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,20 @@
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
+import { Button, HStack, Switch, Text, useColorMode } from '@chakra-ui/react'
+import React from 'react'
+
+const ColorModeSwitch = () => {
+    const { colorMode, toggleColorMode } = useColorMode()
+
+
+    return (
+
+        <HStack >
+            {colorMode == 'dark' ?
+                <SunIcon onClick={toggleColorMode} cursor={'pointer'}/> : <MoonIcon onClick={toggleColorMode}  cursor={'pointer'}/>
+            }
+
+        </HStack>
+    )
+}
+
+export default ColorModeSwitch
